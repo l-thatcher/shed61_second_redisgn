1aba924371bce6e05a91c72d7d882646d1786b65
l-thatcher <louis@thatchermob.com>
2023-05-30 20:22:55 +0100

    fixed some errors for deployments

--- app/components/project.tsx
+++ app/components/project.tsx
@@ -3,6 +3,9 @@ import Image from 'next/image'
 import quote from "/public/quote.png";
 import Link from 'next/link';
 
+import { StaticImageData } from "next/image"
+
+
 interface ProjectProps {
   imageSrc: StaticImageData;
   title: string;
@@ -78,7 +81,7 @@ export default function Project({ imageSrc, title, platform, owner, quoteText, a
           />
         </div>
         <div className='flex justify-center'>
-        <Link href="https://www.lymingtondecorating.com" target="_blank" rel="noopener noreferrer" className='w-full flex justify-center'>
+        <Link href={link} target="_blank" rel="noopener noreferrer" className='w-full flex justify-center'>
             <button
               type="button"
               className="w-3/4 mt-5 mx-auto text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-5 py-2.5 text-center"> view site
