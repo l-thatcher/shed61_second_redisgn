e996d6e58f508edaa45b2b23628d097f640005cc
l-thatcher <louis@thatchermob.com>
2023-05-30 18:16:06 +0100

    added past projects carousell

---
+++ app/components/project.tsx
@@ -0,0 +1,96 @@
+import Image from 'next/image'
+
+import quote from "/public/quote.png";
+import Link from 'next/link';
+
+interface ProjectProps {
+  imageSrc: StaticImageData;
+  title: string;
+  platform: string;
+  owner: string;
+  quoteText: string;
+  aboutText: string;
+  link: string;
+}
+
+export default function Project({ imageSrc, title, platform, owner, quoteText, aboutText, link } : ProjectProps) {
+  return (
+    <div className='w-full h-[70vh] flex flex-row items-center'>
+
+      {/* <div className='w-1/3 h-full flex-col justify-between px-10  bg-slate-600'>
+        // <div className='flex-auto'>
+        //   <p className='pb-3 shrink mb-2 text-3xl font-bold tracking-tight text-white'>{title}</p>
+        //   <p className='py-1 shrink mb-2 text-md tracking-tight text-gray-400'>platform: {platform}</p>
+        //   <p className='shrink mb-2 text-md tracking-tight text-gray-400'>owner: {owner}</p>
+        // </div>
+        
+        <div className='flex flex-col justify-between'>
+          <div className='flex flex-col'>
+            <div className='pt-14 mx-auto'>
+              <Image
+                className='py-6'
+                height={80}
+                src={quote}
+                alt={'quote'}
+              />
+            </div>
+            <p className='shrink mb-2 text-lg font- tracking-tight text-white text-center'>{quoteText}</p>
+          </div>
+        </div>
+
+        <div className='flex justify-center'>
+          <button type="button" className="w-2/3 mx-auto mt-20 text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-md px-5 py-2.5 text-center">view site</button>
+        </div>
+      </div> */}
+
+      <div className='w-1/3 h-full flex flex-col justify-around bg-slate-600 bg-opacity-50 backdrop-blur-sm p-10 rounded-e-xl'>
+        <div className='flex-auto'>
+          <p className='pb-3 shrink mb-2 text-3xl font-bold tracking-tight text-white'>{title}</p>
+          <p className='py-1 shrink mb-2 text-md tracking-tight text-orange-500'>platform: {platform}</p>
+          <p className='shrink mb-2 text-md tracking-tight text-orange-500'>owner: {owner}</p>
+        </div>
+
+        <div className='flex flex-col justify-between'>
+          <div className='flex flex-col'>
+            <div className='pt-14 mx-auto'>
+              <Image
+                className='py-6'
+                height={60}
+                src={quote}
+                alt={'quote'}
+              />
+            </div>
+          <p className='pl-5 mb-2 text-lg font- tracking-tight text-white text-center'>{quoteText}</p>
+        </div>
+        </div> 
+
+        <div className='h-full'></div>
+    
+      </div>
+
+      <div className='w-1/2 h-[70vh] flex-col justify-between'>
+        <div className='flex px-5'>
+          <Image
+            className='shadow-[0_0_50px_0_#202020]'
+            objectFit='contain'
+            src={imageSrc}
+            alt={title}
+          />
+        </div>
+        <div className='flex justify-center'>
+        <Link href="https://www.lymingtondecorating.com" target="_blank" rel="noopener noreferrer" className='w-full flex justify-center'>
+            <button
+              type="button"
+              className="w-3/4 mt-5 mx-auto text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-5 py-2.5 text-center"> view site
+            </button>
+          </Link>
+        </div> 
+      </div>
+
+      <div className='w-1/3 h-full flex flex-col justify-around text-center bg-slate-600 bg-opacity-50 backdrop-blur-sm p-10 rounded-s-xl'>
+        <p className='p-3 text-3xl font-bold tracking-tight text-white'>about</p>
+        <p className='py-1 pr-5 mb-4 text-lg font- tracking-tight text-white'>{aboutText}</p>
+      </div>
+    </div>
+  );
+}
--- app/pages/homePage.tsx
+++ app/pages/homePage.tsx
@@ -27,6 +27,10 @@ import mobile from "/public/mobileimg.png";
 import bespoke from "/public/bespokeimg.png";
 import ai from "/public/aiimg.png";
 
+import projectsbg from "/public/projectsbg4.png";
+
+import paintingByNumbers from "/public/paintingByNumbersHome.png";
+import forestPilates from "/public/forestPilatesHome.png";
 
 
 import {Parallax, ParallaxLayer, IParallax} from '@react-spring/parallax';
@@ -35,11 +39,37 @@ import {Parallax, ParallaxLayer, IParallax} from '@react-spring/parallax';
 import Navbar from '../components/navbar';
 import Services from '../components/services'
 import { start } from 'repl';
+import Project from '../components/project';
+import { link } from 'fs';
 
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
+import { FaArrowCircleLeft, FaArrowCircleRight } from 'react-icons/fa';
+
+
+const paintingByNumbersData = {
+  imageSrc: paintingByNumbers,
+  title: 'painting by numbers',
+  platform: 'wix',
+  owner: 'paul',
+  quoteText: 'Louis has done a great job of bringing my old business website out of the dark ages, I am so pleased with the results. He has provided me with clear advice throughout without ever confusing me with technical jargon. I cannot recommend his services highly enough.',
+  aboutText: "Developing a painting and decorating site using Wix has was an efficient and rewarding experience. In around 10 hours, I was created a visually stunning website that perfectly showcased the client's expertise and past projects. With Wix's user-friendly interface and intuitive drag-and-drop tools, the client is able to upload new projects as and when they like without my help. The platform has pre-built templates, and Wix's seamless integration with various third-party applications and plugins further enhanced the functionality of the website, providing a seamless browsing experience for visitors. Overall, I was able to deliver a remarkable painting and decorating site efficiently and effectively that the client was very happy with.",
+  link: 'https://www.lymingtondecorating.com'
+};
+
+const forestPilatesData = {
+  imageSrc: forestPilates,
+  title: 'forest pilates',
+  platform: 'squarespace',
+  owner: 'lynda',
+  quoteText: 'I would highly recommend Shed61 - I run a small business in the New Forest and needed to update and tweak my website. Louis was able to take my ‘mind map’ of ideas and form them into a cohesive website solution. His patient and consultative manner made what I had avoided for far to long into a painless and dare I say it enjoyable experience! He took my old outdated looking website and modernised its appearance and functionality.',
+  aboutText: "I recently had the pleasure of using SquareSpace to redesign a Pilates website for a client, and the outcome exceeded expectations. By harnessing the platform's intuitive interface and a wide range of customizable templates, I transformed their outdated site into a visually captivating and user-friendly platform. The new design not only enhanced the aesthetics but also improved functionality, making it easier for visitors to navigate and explore the various Pilates offerings. The client was thrilled with the result as the revamped website attracted more visitors and ultimately led to an increase in client inquiries and business growth. SquareSpace proved to be an invaluable tool in helping me deliver exceptional results for my client's Pilates business.",
+  link: 'https://www.forestpilates.co.uk/'
+};
 
 export default function HomePage() {
   const parallax = useRef<IParallax>(null!)
-
 
   return (
     <main className="bg-[#272B4A]">
@@ -54,7 +84,9 @@ export default function HomePage() {
                 <li >
                   <p className="text-white text-md font-w300" onClick={() => parallax.current.scrollTo(1.4)}>services</p>
                 </li>
-
+                <li >
+                  <p className="text-white text-md font-w300" onClick={() => parallax.current.scrollTo(4.45)}>portfolio</p>
+                </li>
                 <li>
                   <div>
                   <Image className=''
@@ -68,12 +100,17 @@ export default function HomePage() {
 
                 <li>
                   <Link href="/about">
+                    <p className="text-white text-md font-w400">skills</p>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/about">
                     <p className="text-white text-md font-w400">about</p>
                   </Link>
                 </li>
                 <li>
                   <Link href="/portfolio">
-                    <p className="text-white text-md font-w500">portfolio</p>
+                    <p className="text-white text-md font-w500">contact</p>
                   </Link>
                 </li>
               </ul>
@@ -83,7 +120,7 @@ export default function HomePage() {
       </div>
 
       <div className="flex justify-center h-screen">
-        <Parallax ref={parallax} pages={5}>
+        <Parallax ref={parallax} pages={8}>
           <ParallaxLayer offset={0} speed={0.1}>
             <Image className=''
                 priority
@@ -239,6 +276,55 @@ export default function HomePage() {
             </div>
           </ParallaxLayer>
 
+          <ParallaxLayer offset={4.5} speed={0} id='projects'> 
+          <Image className='w-full'
+                priority
+                src={projectsbg}
+                alt="projectsBG"
+              />
+          </ParallaxLayer>
+          <ParallaxLayer offset={4.6} speed={0.2}>
+            <div className='w-full text-center'>
+                <h1 className='text-5xl text-white py-10'>what we've done</h1>
+            </div>
+            
+            <Carousel autoPlay={true} infiniteLoop={true}
+            renderArrowPrev={(clickHandler, hasPrev) => {
+              return (
+                <div
+                  className={`${
+                    hasPrev ? "absolute" : "hidden"
+                  } top-0 bottom-0 left-0 flex justify-center items-center p-3 opacity-30 hover:opacity-100 cursor-pointer z-20 text-white text-2xl`}
+                  onClick={clickHandler}
+                >
+                  <FaArrowCircleLeft/>
+                </div>
+              );
+            }}
+            renderArrowNext={(clickHandler, hasNext) => {
+              return (
+                <div
+                  className={`${
+                    hasNext ? "absolute" : "hidden"
+                  } top-0 bottom-0 right-0 flex justify-center items-center p-3 opacity-30 hover:opacity-100 cursor-pointer z-20 text-white text-2xl`}
+                  onClick={clickHandler}
+                >
+                  <FaArrowCircleRight/>
+                </div>
+              );
+            }}>
+                <div>
+                  <Project {...paintingByNumbersData}></Project>
+                </div>
+                <div>
+                  <Project {...forestPilatesData}></Project>
+                </div>
+            </Carousel>
+
+            <Link href={'#contact'} className='w-full flex justify-center mt-10'>
+              <button type="button" className="w-1/2 mx-auto text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-xl px-5 py-2.5 text-center">get in touch</button>
+            </Link>
+          </ParallaxLayer>
         </Parallax>
       </div>
     </main>
